e571a9425009f77bfea886302ca6f50205634a80
David Vincelli <david.vincelli@gmail.com>
2023-03-18 13:44:12 -0400

    WIP: example 2.1 of "The Nature of Code" in rust and SDL2.

---
+++ src/main.rs
@@ -0,0 +1,82 @@
+extern crate sdl2;
+
+mod rvector;
+mod mover;
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
+
+use rand::prelude::*;
+use crate::rvector::RVector;
+use crate::mover::Mover;
+
+const WIDTH: u32 = 1024;
+const HEIGHT: u32 = 768;
+
+
+pub fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("Example 2.1: Forces", WIDTH.try_into().unwrap(), HEIGHT.try_into().unwrap())
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut movers : Vec<Mover> = Vec::<Mover>::with_capacity(20);
+
+    let mut wind = RVector::new2d(0.01, 0.0);
+    let gravity = RVector::new2d(0.0, 0.1);
+
+    for i in 0..movers.capacity() {
+        let x = (WIDTH as f32) * rand::random::<f32>();
+        let y = (HEIGHT as f32) * rand::random::<f32>();
+        movers.push(
+            Mover::new(
+                    rand::random::<f32>() * 5.0 + 1.0,
+                    0.0,
+                    0.0
+                )
+            )
+    }
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    'running: loop {
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+
+        for m in &mut movers {
+            m.apply_force(&wind);
+            let gravity = RVector::new2d(0.0, 0.1 * m.mass);
+            m.apply_force(&gravity);
+            m.update();
+            m.display(&mut canvas);
+            m.check_edges((WIDTH as f32), (HEIGHT as f32));
+        }
+
+        if rand::random() {
+            wind.set2d(rand::random::<f32>() * 0.02, 0.0);
+        }
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
---
+++ src/mover.rs
@@ -0,0 +1,88 @@
+use rand::prelude::*;
+
+use crate::RVector;
+
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+use sdl2::pixels::Color;
+use crate::sdl2::gfx::primitives::DrawRenderer;
+
+
+#[derive(Debug)]
+pub struct Mover {
+    pub location: RVector,
+    pub velocity: RVector,
+    pub acceleration: RVector,
+    pub mass: f32,
+}
+
+impl Mover {
+    pub fn new(mass: f32, x: f32, y: f32) -> Self {
+        Mover {
+            location: RVector::new2d(x, y),
+            velocity: RVector::new2d(0.0, 0.0),
+            acceleration: RVector::new2d(0.0, 0.0),
+            mass: mass
+        }
+    }
+
+    pub fn apply_force(&mut self, force: &RVector) {
+        let mut force = force.clone();
+        force.div(self.mass);
+        self.acceleration.add(&force);
+    }
+
+    pub fn update(&mut self) {
+        self.velocity.add(&self.acceleration);
+        self.location.add(&self.velocity);
+        self.acceleration.mult(0.0);
+    }
+
+    pub fn display(&self, canvas: &mut Canvas<Window>) {
+        canvas.ellipse(
+            (self.location.x as i16).try_into().unwrap(),
+            (self.location.y as i16).try_into().unwrap(),
+            ((self.mass * 16.0) as i16).try_into().unwrap(),
+            ((self.mass * 16.0) as i16).try_into().unwrap(),
+            Color::RGB(255, 0, 0)
+        );
+    }
+
+    pub fn check_edges(&mut self, width: f32, height: f32) {
+        if self.location.x > width {
+            self.location.x = width;
+            self.velocity.x *= -1.0;
+        } else if self.location.x < 0.0 {
+            self.velocity.x *= -1.0;
+            self.location.x = 0.0;
+        }
+
+        if self.location.y > height {
+            self.location.y = height;
+            self.velocity.y *= -1.0;
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mover() {
+        let mut m1 = Mover::new(rand::random(), rand::random());
+        let mut m2 = Mover::new(rand::random(), rand::random());
+
+        let wind = RVector::new2d(1.0, 0.0);
+
+        m1.apply_force(&wind);
+        m2.apply_force(&wind);
+
+        assert_eq!(m1.acceleration.x, 0.1);
+        assert_eq!(m1.acceleration.y, 0.0);
+
+        assert_eq!(m2.acceleration.x, 0.1);
+        assert_eq!(m2.acceleration.y, 0.0);
+    }
+}
